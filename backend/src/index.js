e8d462607131ea7ffe685eb3fb31876467a5ebc0
reachmohdaltaf <reachmohdaltaf@gmail.com>
2025-01-02 20:14:53 +0530

    problem dist

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,10 +24,11 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/dist')))
-    app.get('*', (req, res)=>{
-        res.sendFile(path.join(__dirname, '/frontend', 'dist', 'index.html'))
-    })
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+
 }
 
 server.listen(PORT, ()=>{
