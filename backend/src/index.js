ee7ae69f6ea06ed23db95b8558e81ce5e398e4fd
reachmohdaltaf <reachmohdaltaf@gmail.com>
2025-01-02 20:53:37 +0530

    problem pathhh

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,13 @@ import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import { ConnectDB } from './lib/db.js'
 import cors from 'cors'
-import { app, server } from './lib/socket.js'
+import {app, server} from './lib/socket.js'
 import path from 'path'
 dotenv.config()
 
 const PORT = process.env.PORT
 const __dirname = path.resolve()
+
 
 app.use(cookieParser())
 app.use(express.json())
@@ -23,16 +24,15 @@ app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
-    // Correct the path to the frontend build folder
-    const frontendPath = path.join(__dirname, "../frontend/dist");
-    app.use(express.static(frontendPath));  // Serve static files
-
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  
     app.get("*", (req, res) => {
-        res.sendFile(path.join(frontendPath, "index.html"));  // Serve index.html from 'dist' folder
+      res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
     });
-}
+  }
 
-server.listen(PORT, () => {
+
+server.listen(PORT, ()=>{
     ConnectDB()
-    console.log('Server is running on port ' + PORT)
+    console.log('server is running on port PORT:' + PORT)
 })
